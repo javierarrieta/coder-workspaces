b8729819c6db839feaa619151db856cbbc4b77f1
javierarrieta <javier.arrieta@gmail.com>
2026-08-20 16:58:00 +0000

    feat: initial coder-workspace image flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Coder workspace container images";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      packages.x86_64-linux.default = import ./image.nix { inherit pkgs; };
+      packages.x86_64-linux.coder-workspace = self.packages.x86_64-linux.default;
+      formatter.x86_64-linux = pkgs.nixfmt-tree;
+    };
+}
